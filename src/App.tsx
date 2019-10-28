677ea18a6c335b9ea4cce13137ae0c0af291afc5
Amazing <naturalnoneedy@gmail.com>
2019-10-28 04:52:43 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import PokemonSearch from './components/PokemonSearch';
 
-const App: React.FC = () => {
-  return (
+class App extends Component {
+
+ 
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PokemonSearch name='API' numberOfPokemons={966}/>
+      {/* TODO: make count dynamic */}
     </div>
-  );
+  )
+}
 }
 
 export default App;
